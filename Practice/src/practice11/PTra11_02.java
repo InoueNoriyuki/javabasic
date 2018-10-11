bd8e363e526ae2ccd3ad8e43fdcea51a99ca3bb1
LAPTOP-IUU3QLB9\ruatt <ruattsan@gmail.com>
2018-10-11 15:25:01 +0900

    11-2

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -10,11 +10,16 @@ public class PTra11_02 {
 	public static void main(String[] args) {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
+		FileReaderClass.readBookDataFile();
 
 
 		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
+		Book book = new Book("スッキリわかるJava入門","中山清喬／国本大悟",2600,657, "インプレス");
+
+
+
 
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
-
+		System.out.println(book.dispBookInfo());
 	}
 }
