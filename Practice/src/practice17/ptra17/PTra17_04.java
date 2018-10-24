2617e627e6165db01cc21bd0ccfc07de4a7e307d
LAPTOP-IUU3QLB9\ruatt <ruattsan@gmail.com>
2018-10-24 15:46:04 +0900

    ~17-4

--- Practice/src/practice17/ptra17/PTra17_04.java
+++ Practice/src/practice17/ptra17/PTra17_04.java
@@ -9,12 +9,23 @@ package practice17.ptra17;
 public class PTra17_04 {
 	public static void main(String[] args) {
 
-		String str = "スッキリわかるJava入門";
+
 
 		/*
 		 * ★ strに入っている文字列を1文字ずつゆっくり表示させるプログラムを完成させてください
 		 * ※ Thread.sleep を使用すること
 		 */
 
+		char[] str =  new char[0];
+		char[] = "スッキリわかるjava";
+		for (int i = 0; i < str.length; i++) {
+			System.out.print(str[i]);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				/* エラーが発生した場合はここにくる */
+
+	}
+		}
 	}
 }
